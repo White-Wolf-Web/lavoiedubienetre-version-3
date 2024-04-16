eee56468f7da2e63407a75acac4568be15bb6257
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-04-16 20:29:01 +0200

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,43 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'lavoiedubienetre.be',
+            }
+          ],
+          permanent: true,
+          destination: 'https://www.lavoiedubienetre.be/:path*'
+        },
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'www.lavoiedubienetre.be',
+            }
+          ],
+          permanent: true,
+          destination: 'https://www.lavoiedubienetre.be/:path*'
+        },
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: '(?!www\\.lavoiedubienetre\\.be).*'
+            }
+          ],
+          permanent: true,
+          destination: 'https://www.lavoiedubienetre.be/:path*'
+        }
+      ]
+    },
+  }
+  
+  module.exports = nextConfig;
+  
