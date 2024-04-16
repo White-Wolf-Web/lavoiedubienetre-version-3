b3c690d9d1e0e2240f939c798e9f816987061bcb
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-04-16 20:48:11 +0200

    image

--- src/components/card/MassageCard.tsx
+++ src/components/card/MassageCard.tsx
@@ -17,11 +17,12 @@ export default function MassageCard({ title, description, imageUrl, imageAlt, ur
         <div className="w-full sm:w-1/2 md:w-1/3 px-4 mb-8">
           <div className="bg-white shadow-lg rounded-lg overflow-hidden">
             <Link href={url} >
-            <Image src={imageUrl} alt={imageAlt} width={width} height={height} layout="responsive" title={titleImg} /></Link>
+            <Image src={imageUrl} alt={imageAlt} width={width} height={height} layout="responsive" title={titleImg} />
             <div className="p-1">
               <h3 className="text-xl text-black font-semibold mb-2">{title}</h3>
               <p className="text-gray-600">{description}</p>
             </div>
+            </Link>
           </div>
         </div>
       );
