e62d8dec7475196821211b912f3606e375df4cda
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-04-16 19:33:36 +0200

    Update MassageCardContainer.tsx

--- src/components/card/MassageCardContainer.tsx
+++ src/components/card/MassageCardContainer.tsx
@@ -9,6 +9,7 @@ interface massageCardData {
 	imageUrl: string;
 	imageAlt: string;
 	url: string;
+	titleImg:string;
 }
 
 export default function MassageCardContainer() {
@@ -19,7 +20,7 @@ export default function MassageCardContainer() {
 				<h2 className="text-3xl md:text-4xl leading-snug mb-4">Soins thérapeutiques</h2>
 					<div className="flex flex-wrap -mx-4">
 						{MassageCarData.map((card: massageCardData) => (
-							<MassageCard key={card.id} title={card.title} description={card.description} imageUrl={card.imageUrl} imageAlt={card.imageAlt}  url={card.url} />
+							<MassageCard key={card.id} title={card.title} description={card.description} imageUrl={card.imageUrl} imageAlt={card.imageAlt}  url={card.url} titleImg={card.titleImg} />
 						))}
 					</div>
 				</div>
