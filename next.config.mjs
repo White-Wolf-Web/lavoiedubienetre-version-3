f462b2c891426ba2384284f3259b759c4e357ba5
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-04-16 20:38:56 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/:path*',
-          permanent: true,
-          destination: 'https://www.lavoiedubienetre.be/:path*'
-        },
-       
-      ]
-    },
-  }
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
