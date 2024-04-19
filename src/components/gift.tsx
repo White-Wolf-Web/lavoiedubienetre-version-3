9260c67e3d5ca0c432e383403ae6223cdfc2414e
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-04-19 20:07:43 +0200

    Update gift.tsx

--- src/components/gift.tsx
+++ src/components/gift.tsx
@@ -1,7 +1,7 @@
 
 export default function Gift() {
   return (
-<section className="border border-emerald-500 rounded p-2.5 mt-3.75" >
+<section className="border border-emerald-500 rounded p-2.5 mt-5" >
     <h2>Offrez un bon cadeau pour un massage de votre choix</h2>
     <p>C&apos;est le cadeau parfait pour permettre à vos proches de libérer le stress et de jouir d&apos;une relaxation intense. </p>
     <p>Encouragez la détente et la relaxation chez vos amis ou collègues avec nos chèques cadeaux pour une séance de massage, disponible du lundi au samedi.</p>
