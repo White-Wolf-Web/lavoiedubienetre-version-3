c07134adfdf6ec093ca6d19b6e4539e0d3d34016
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-05-11 17:52:35 +0200

    test metadata banniere

--- src/components/Banniere.tsx
+++ src/components/Banniere.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image"
 import Banner from '../../public/img/banniere-la-voie-du-bien-etre.webp'
-
+import { MetadataBanner } from "./metadata/MetadataBanner"
 
 export default function Banniere() {
   return (
     <div className="mb-0">
+     
     <Image src={Banner} alt="Vue sur un Bouddha de mon salon de massage - shiatsu" className="banner" title="salon de soins relaxant" loading="eager" decoding="async" data-nimg="1" ></Image>
+    <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(MetadataBanner) }} />
     </div>
   )
 }
